524c3275f6f3b9fb492d4a562a8ac703c79f03fb
Vladyslav <vladkalin881@gmail.com>
2026-07-19 12:09:59 +0300

    refactor: ove reusable objects to class fields

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,13 +4,12 @@ import java.util.Random;
 
 public class Lottery {
     private static final int MAX_BALL_NUMBER = 100;
+    private final ColorSupplier supplier = new ColorSupplier();
+    private final Random random = new Random();
 
     public Ball getRandomBall() {
-        ColorSupplier supplier = new ColorSupplier();
         Color color = supplier.getRandomColor();
-        Random random = new Random();
         int number = random.nextInt(MAX_BALL_NUMBER + 1);
-        Ball ball = new Ball(color, number);
-        return ball;
+        return new Ball(color, number);
     }
 }
