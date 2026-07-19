b7dccb3ebe2d243b77df71b6744e154ecfec6f78
Vladyslav <vladkalin881@gmail.com>
2026-07-19 11:55:17 +0300

    fix: Address lottery checklist issues

--- src/main/java/core/basesyntax/Color.java
+++ src/main/java/core/basesyntax/Color.java
@@ -4,5 +4,5 @@ public enum Color {
     RED,
     GREEN,
     BLUE,
-    YELLOY
+    YELLOW
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int MAX_BALL_NUMBER = 100;
+
     public Ball getRandomBall() {
         ColorSupplier supplier = new ColorSupplier();
         Color color = supplier.getRandomColor();
         Random random = new Random();
-        int number = random.nextInt(101);
+        int number = random.nextInt(MAX_BALL_NUMBER + 1);
         Ball ball = new Ball(color, number);
         return ball;
     }
