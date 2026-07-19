22aedbb5ba4d149774b75eaec2ef2bdf14d60947
Vladyslav <vladkalin881@gmail.com>
2026-07-19 11:40:10 +0300

    feat: Create Ball and Lottery classes with random generation

---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    public Ball getRandomBall() {
+        ColorSupplier supplier = new ColorSupplier();
+        Color color = supplier.getRandomColor();
+        Random random = new Random();
+        int number = random.nextInt(101);
+        Ball ball = new Ball(color, number);
+        return ball;
+    }
+}
